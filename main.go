23cd16f2191710abb3ebb503f848022542471860
Judson <nyarly@gmail.com>
2018-11-01 09:59:09 -0700

    Docker image deps

--- main.go
+++ main.go
@@ -9,22 +9,24 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/builder/dockerfile/parser"
 )
 
 type depRecord struct {
-	from     string
+	from     reference.Reference
 	addpaths []string
 }
 
 var records = map[string]depRecord{}
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		log.Fatalf("Usage: %s <build root directory>", os.Args[0])
 	}
 
-	dir := os.Args[1]
+	registryURL := os.Args[1]
+	dir := os.Args[2]
 
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if info.Name() == "Dockerfile" {
@@ -45,11 +47,50 @@ func main() {
 
 	fmt.Println("# file dependencies based on ADD and COPY directives")
 	for name, record := range records {
+		if record.isLocal() {
+			fmt.Printf(".build/%s: .build/%s\n\n", name, record.localFrom(registryURL))
+		} else {
+			remote := record.remoteFrom()
+
+			filename := strings.Replace(remote, "_", "_u_", -1) // ick, but that's how it goes
+			filename = strings.Replace(filename, "/", "_s_", -1)
+			filename = strings.Replace(filename, ":", "_c_", -1)
+			filename = filename + ".image"
+
+			fmt.Printf(".build/%s: .remote/%s\n\n", name, filename)
+			fmt.Printf(".remote/%[1]s: .pull-once | .remote\n"+
+				"\tdocker pull %[2]s\n"+
+				"\t@docker image inspect %[2]s --format '{{.Id}}' > $(TMPDIR)/%[1]s\n"+
+				"\t@diff $(TMPDIR)/%[1]s $@ \\\n\t  || mv $(TMPDIR)/%[1]s $@\n\n", filename, remote)
+		}
+
 		fmt.Printf("build-all: build-%s\n\n", name)
 		fmt.Printf("push-all: push-%s\n\n", name) // leaves?
 		fmt.Printf(".build/%s: | .build\n\n", name)
 		fmt.Printf(".build/%s: %s\n\n", name, strings.Join(record.filedeps(dir, name), " "))
 	}
+}
+
+func (r depRecord) isLocal() bool {
+	if t, is := r.from.(reference.Tagged); is && t.Tag() == "local" {
+		return true
+	}
+	return false
+}
+
+func (r depRecord) localFrom(url string) string {
+	if n, is := r.from.(reference.Named); is {
+		if strings.Index(n.Name(), url) != 0 {
+			log.Fatalf("FROM %s uses a registry URL != %q", n.String(), url)
+			return n.Name()[len(url):]
+		}
+	}
+	log.Fatalf("FROM %s doesn't parse as a named Docker reference", r.from.String())
+	return ""
+}
+
+func (r depRecord) remoteFrom() string {
+	return r.from.String()
 }
 
 func (r depRecord) filedeps(base, name string) []string {
@@ -107,7 +148,11 @@ func extractDeps(ast *parser.Node) depRecord {
 	for _, node := range ast.Children {
 		switch node.Value {
 		case "from":
-			record.from = node.Next.Value
+			var err error
+			record.from, err = reference.Parse(node.Next.Value)
+			if err != nil {
+				log.Fatal(err)
+			}
 		case "add", "copy":
 			path := node.Next.Value
 			if chownRE.MatchString(path) {
